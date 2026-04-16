d66f8e25355574f1a877989c5c9f0cef252db011
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-04-16 04:31:50 -0700

    deploy vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Project_Color_Palette_Genrator/",
   plugins: [tailwindcss(), react()],
 });
